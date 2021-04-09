3a8e3eb55da6ed7fa1dcb9c6699b2b4084d19fc3
jasmineanica <jasmine.tea.fragrance@gmail.com>
2021-04-09 10:40:39 -0700

    setting up Express and Mongodb

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) =>
+          res.json({ msg: 'Welcome to the ContactKeeper API' }));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
